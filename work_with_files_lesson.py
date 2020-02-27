a568e150ecb50305e1bb6af2e060a6bbca814016
Blinov Pavel <sarenkirovnet@gmail.com>
2020-02-27 20:00:59 +0300

    First Version

---
+++ work_with_files_lesson.py
@@ -0,0 +1,17 @@
+
+
+def file_read():
+    file = 'referat.txt'
+
+    with open(file, encoding='utf-8') as f:
+        content = f.read()
+    print(f'Line length is: {len(content)}.')
+    words = content.split()
+    print(f'Amount of words is: {len(words)}.')
+    content_replace = content.replace('.', '!')
+    with open('referat_2.txt', 'w', encoding='utf-8') as f:
+        f.write(content_replace)
+
+
+if __name__ == '__main__':
+    file_read()
