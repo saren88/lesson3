cb6c8c4673376cafc36257632c235a3aac2ecbf5
Blinov Pavel <sarenkirovnet@gmail.com>
2020-02-29 14:55:08 +0300

    Refactored Version

--- file_read_rewrite_lesson.py
+++ file_read_rewrite_lesson.py
@@ -1,5 +1,4 @@
 
-
 def file_read():
     file = 'referat.txt'
 
@@ -8,6 +7,13 @@ def file_read():
     print(f'Line length is: {len(content)}.')
     words = content.split()
     print(f'Amount of words is: {len(words)}.')
+
+
+def file_rewrite():
+    file = 'referat.txt'
+
+    with open(file, encoding='utf-8') as f:
+        content = f.read()
     content_replace = content.replace('.', '!')
     with open('referat_2.txt', 'w', encoding='utf-8') as f:
         f.write(content_replace)
@@ -15,3 +21,4 @@ def file_read():
 
 if __name__ == '__main__':
     file_read()
+    file_rewrite()
