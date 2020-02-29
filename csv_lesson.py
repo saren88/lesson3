9383edcdd0dd8539f3209e62fe68303d1bda0843
Blinov Pavel <sarenkirovnet@gmail.com>
2020-02-29 14:48:15 +0300

    Renamed and refactored

--- csv_lesson.py
+++ csv_lesson.py
@@ -1,20 +1,19 @@
 import csv
 
 
-def user_list():
+def csv_lesson():
     users = [
-            {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
-            {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
-            {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
         ]
-
-    with open('export.csv', 'w', encoding='utf-8', newline='') as f:
-        fields = ['name', 'age', 'job']
-        writer = csv.DictWriter(f, fields, delimiter=';')
+    fields = ['name', 'age', 'job']
+    with open('export.csv', 'w', encoding='utf-8', newline='') as users_list:
+        writer = csv.DictWriter(users_list, fields, delimiter=';')
         writer.writeheader()
         for user in users:
             writer.writerow(user)
 
 
 if __name__ == '__main__':
-    user_list()
+    csv_lesson()
